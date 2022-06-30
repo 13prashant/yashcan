808cae3dd7b4b0db2b90280e618145c0d1454354
13prashant <pacific1392@gmail.com>
2022-06-30 15:17:00 +0530

    created header

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,5 +1,65 @@
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
 const Navbar = () => {
-    return <div>Navbar</div>;
+    const router = useRouter();
+    const isPathHome = router.pathname === "/";
+    const isPathOurBusiness = router.pathname === "/our-business";
+    const isPathProducts = router.pathname === "/products";
+    const isPathContactUs = router.pathname === "/contact-us";
+
+    const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+    return (
+        <header>
+            <div className="container">
+                <nav className="navbar">
+                    <a href="/" className="navbar__logo">
+                        <h3>YASHCAN</h3>
+                    </a>
+                    <ul style={{ display: isMenuVisible ? "flex" : "none" }}>
+                        <li
+                            className={`navbar__link ${
+                                isPathHome ? "active" : ""
+                            }`}
+                        >
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li
+                            className={`navbar__link ${
+                                isPathOurBusiness ? "active" : ""
+                            }`}
+                        >
+                            <Link href="/our-business">Our Business</Link>
+                        </li>
+                        <li
+                            className={`navbar__link ${
+                                isPathProducts ? "active" : ""
+                            }`}
+                        >
+                            <Link href="/products">Products</Link>
+                        </li>
+                        <li
+                            className={`navbar__link ${
+                                isPathContactUs ? "active" : ""
+                            }`}
+                        >
+                            <Link href="/contact-us">Contact Us</Link>
+                        </li>
+                    </ul>
+                    <div
+                        onClick={() => setIsMenuVisible(!isMenuVisible)}
+                        className="navbar__hamburger"
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    );
 };
 
 export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
-import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
             <Navbar />
-            <Component {...pageProps} />
+            <div className="container">
+                <Component {...pageProps} />
+            </div>
             <Footer />
         </>
     );

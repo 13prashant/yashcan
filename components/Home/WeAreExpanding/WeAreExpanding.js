a9f8f0690411fe26c7957034f281fe3e3a27edd5
13prashant <pacific1392@gmail.com>
2025-06-22 13:53:33 +0530

    fixed console warnings

--- components/Home/WeAreExpanding/WeAreExpanding.js
+++ components/Home/WeAreExpanding/WeAreExpanding.js
@@ -26,24 +26,24 @@ const WeAreExpanding = () => {
                         >
                             <path d="M0 488L0 171.3c0-26.2 15.9-49.7 40.2-59.4L308.1 4.8c7.6-3.1 16.1-3.1 23.8 0L599.8 111.9c24.3 9.7 40.2 33.3 40.2 59.4L640 488c0 13.3-10.7 24-24 24l-48 0c-13.3 0-24-10.7-24-24l0-264c0-17.7-14.3-32-32-32l-384 0c-17.7 0-32 14.3-32 32l0 264c0 13.3-10.7 24-24 24l-48 0c-13.3 0-24-10.7-24-24zm488 24l-336 0c-13.3 0-24-10.7-24-24l0-56 384 0 0 56c0 13.3-10.7 24-24 24zM128 400l0-64 384 0 0 64-384 0zm0-96l0-80 384 0 0 80-384 0z" />
                         </svg>
-                        <h1>We&apos;re Expanding!</h1>
-                        <p>
+                        <Dialog.Title className="title">
+                            We&apos;re Expanding!
+                        </Dialog.Title>
+                        <Dialog.Description className="description">
                             YashCan Incorporated proudly announces the opening
                             of our new warehouse at <br />
                             <strong>
                                 2415 Kishwaukee Street, Rockford, Illinois
                                 61104.
-                            </strong>
-                        </p>
-                        <p>
+                            </strong>{" "}
+                            <br />
                             We specialize in premium plastic grades – HDPE,
                             LDPE, PP, and PET, serving the recycling and
                             manufacturing industries.
-                        </p>
-                        <p>
+                            <br /> <br />
                             Quality materials | Reliable supply | Endless
                             possibilities.
-                        </p>
+                        </Dialog.Description>
                         <div
                             className="weAreExpanding__close"
                             onClick={() => setOpen(false)}
--- components/Reusables/Logo/Logo.js
+++ components/Reusables/Logo/Logo.js
@@ -1,23 +1,30 @@
 import PropTypes from "prop-types";
 
 const Logo = ({ inverted, ...rest }) => {
-    const logoStyle = {
-        display: "flex",
-        cursor: "pointer",
-    };
-    const yashStyle = {
-        color: "#ffffff",
-        backgroundColor: "#2e3b95",
-    };
-    const canStyle = {
-        color: inverted ? "#000000" : "",
-        backgroundColor: inverted ? "#f26223" : "",
-    };
-
     return (
-        <div style={logoStyle} {...rest}>
-            <h2 style={yashStyle}>Yash</h2>
-            <h2 style={canStyle}>Can</h2>
+        <div
+            style={{
+                display: "flex",
+                cursor: "pointer",
+            }}
+            {...rest}
+        >
+            <h2
+                style={{
+                    color: "#ffffff",
+                    backgroundColor: "#2e3b95",
+                }}
+            >
+                Yash
+            </h2>
+            <h2
+                style={{
+                    color: "#000000",
+                    backgroundColor: inverted ? "#f26223" : "transparent",
+                }}
+            >
+                Can
+            </h2>
         </div>
     );
 };
